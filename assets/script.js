cd5a0f291b998635642c823dfb2b24f885673eb4
Twyll <twyllblackleaf@gmail.com>
2021-05-14 20:32:57 -0400

    start page and basic styling

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,15 @@
+var mainTextEl = document.querySelector("#main-text");
+var mainContentEl = document.querySelector("#main-content");
+var timeNumberEl = document.querySelector("#timer-number");
+
 var startScreen = function() {
-    
+    mainTextEl.textContent = "Coding Quiz Challenge";
+    mainContentEl.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
+    timeNumberEl.textContent = "0";
+
+    var startButtonEl = document.createElement("button");
+    startButtonEl.textContent = "Start Quiz";
+    mainContentEl.appendChild(startButtonEl);
 }
+
+startScreen();
